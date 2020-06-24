bf9c002f9e57e8b5dfd60cfa084404561d552176
“zhangyuejian” <zhyj_su@163.com>
2020-06-24 13:48:56 +0800

    dev

--- flyway/src/test/java/com/zero/flyway/FlywayApplicationTests.java
+++ flyway/src/test/java/com/zero/flyway/FlywayApplicationTests.java
@@ -1,13 +1,24 @@
 package com.zero.flyway;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FlywayApplicationTests {
+    @Autowired
+    StringEncryptor stringEncryptor;
 
     @Test
     void contextLoads() {
+    }
+
+
+    @Test
+    public void encryptPwd() {
+        String result = stringEncryptor.encrypt("123456");
+        System.out.println(result);
     }
 
 }
